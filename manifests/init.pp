c30028eec8b4325e1fb4340224119b0142ac2a95
Aaron Russo <arusso@berkeley.edu>
2013-05-01 17:30:27 -0700

    working state, but needs some more work before ready for primetime

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,20 @@
 # == Class: xinetd
-# Setups xinetd so we can begin adding packages to it
+#
+# Setup xinetd so we can begin adding services behind it
 #
 # === Parameters:
-# [*service_enable*]
-# Should we enable the service on boot?
 #
-#   service_enable = <true|false>
+# [*service_enable*]
+#
+# Determine whether we should start the service on boot.  Valid values are
+# true, false or 'undef'. Default is true.
 #
 # [*service_ensure*]
-# Should we ensure the service is in a particular state?
 #
-#   service_ensure => <'running'|'stopped'|undef>
+# Determine if we should ensure the service is in a particular state. Valid
+# values are 'running', 'stopped' or 'undef'. Default is running.
 #
-class xinetd (
-  $service_enable = true,
-  $service_ensure = running
-) {
-  # validate inputs yo
-  if $service_ensure != undef { validate_re( '(?i)^(running|stopped)$' ) }
-  if $service_enable != undef { validate_bool( $service_enable ) }
-
-  $service_ensure_r = $service_ensure
-  $service_enable_r = $service_enable
-
+class xinetd  {
   include xinetd::package, xinetd::service
 
   Class['xinetd'] -> Class['xinetd::package'] ~> Class['xinetd::service']
